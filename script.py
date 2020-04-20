4952b5645f2f6497abc99c1413d57b8eb5ffc518
Cierra Johnson-carter <cierrajohnson-carter@Cierras-Air.lan1>
2020-04-20 16:45:54 -0400

    Added movies list

---
+++ script.py
@@ -0,0 +1,10 @@
+def average_budget():
+    movies = [
+        ('Avengers: Endgame', 400000000),
+        ('Pirates of the Caribbean: On Stranger Tides', 379000000),
+        ('Avengers: Age of Ultron', 365000000),
+        ('Star Wars: Ep. VII: The Force Awakens', 306000000),
+        ('Avengers: Infinity War', 300000000)
+    ]
+    for item in movies[0]:
+        print(item)
