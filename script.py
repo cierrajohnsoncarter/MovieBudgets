a2098a5addfef72104f3df32af01bddfd4873925
Cierra Johnson-carter <cierrajohnson-carter@Cierras-Air.lan1>
2020-04-21 21:06:43 -0400

    Added feature for user to add movies to list

--- script.py
+++ script.py
@@ -8,6 +8,16 @@ movies = [
     ('Avengers: Infinity War', 300000000)
 ]
 
+# Allows user to input how many movies they want to add to the list
+add_new_movie = int(input('How many movies do you want to add? '))
+
+# Takes the name and budget of the movie the user entered and add the movie to the list
+for _ in range(add_new_movie):
+    name = input('Enter movie name: ')
+    budget = input('Enter movie budget: ')
+    new_movie = (name, int(budget))
+    movies.append(new_movie)
+
 # List representing over budget movies and counter to keep track of the total budget of all the movies in the list
 over_budget_movies = []
 total_budget = 0
